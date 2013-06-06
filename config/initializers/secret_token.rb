0ca4cb273b0892a22c352e5fee24fdbf19d0d78e
Kevin Wei <kevin.tw.wei@gmail.com>
2013-06-05 23:54:40 -0700

    placeholder for appaday to get peer reviewer

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helpdesk::Application.config.secret_token = 'e6a785c06219b5892895c59c24fb137ec1b76b68e90f39bd69b4e611a58a1de57e7ce140553626dfdc670574e37bfdcf1ba86e6bda721efd1eea31b8b9951401'
